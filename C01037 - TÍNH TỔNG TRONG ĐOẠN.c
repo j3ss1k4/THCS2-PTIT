3b77f8196c462e0fad94c48f10424bdf60a4cbe1
Jessica <126171159+thaodo6904@users.noreply.github.com>
2023-05-21 21:57:36 +0700

    Create C01037 - TÍNH TỔNG TRONG ĐOẠN.c

---
+++ C01037 - TÍNH TỔNG TRONG ĐOẠN.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int a,b,sum=0;
+	scanf("%d%d",&a,&b);
+	if(a<b)
+		for(int i=a;i<=b;i++)
+			sum+=i;
+	else
+		for(int i=b;i<=a;i++)
+			sum+=i;
+	
+	printf("%d",sum);
+	
+}
